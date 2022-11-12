76294efa84feaf6ab60cb02675dfc179f1013a6b
James Wasmer <jameswasmer@gmail.com>
2022-11-12 16:04:31 -0500

    Add flatpickr to manage date selections

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,7 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import flatpickr from 'flatpickr';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/stanley-hotel-with-mountains.png';
@@ -12,11 +13,15 @@ import './images/double-bed.png';
 import './images/regular-room.png';
 import './images/overlook-logo.png';
 
+const datePicker = document.querySelector(".date-picker");
 const bookingsMenu = document.querySelector('.bookings-menu')
 const bookingsDropdown = document.querySelector('.bookings-dropdown')
+const bookBtn = document.querySelectorAll('.room-card--book-btn')
 
 bookingsMenu.addEventListener('click', toggleMenu)
-bookingsDropdown.addEventListener('click', showClick)
+bookBtn.forEach(button => {
+  addEventListener('click', displayCalendar)
+})
 
 function toggleMenu() {
   if (bookingsDropdown.style.opacity === "0") {
@@ -28,6 +33,10 @@ function toggleMenu() {
   }
 }
 
-function showClick() {
-  console.log('click')
+function displayCalendar() {
+  event.preventDefault()
 }
+
+const booking = flatpickr(bookBtn, {
+  enableTime: false,
+});
