7f6969a37a8110f967eb9296ceb853a8967246f5
James Wasmer <jameswasmer@gmail.com>
2022-11-12 21:05:30 -0500

    Create global initialization variables and assign fetched data

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,4 @@
 import './css/styles.css';
-import flatpickr from 'flatpickr';
-
 import './images/stanley-hotel-with-mountains.png';
 import './images/stanley-sky.png';
 import './images/lodge-suite.png';
@@ -8,7 +6,22 @@ import './images/double-bed.png';
 import './images/regular-room.png';
 import './images/overlook-logo.png';
 
-const datePicker = document.querySelector(".date-picker"); /* recheck when dropdown is done */
+import flatpickr from 'flatpickr';
+
+import { fetchAll } from './apiCalls';
+
+// ********** INITIALIZE **********
+let store = {
+  userData: null,
+  bookingsData: null,
+}
+
+fetchAll()
+  .then((data) => {
+    store.userData = loginUser(data.usersData)
+    store.bookingsData = data.bookingsData
+  })
+
 const bookingsMenu = document.querySelector('.bookings-menu')
 const bookingsDropdown = document.querySelector('.bookings-dropdown')
 const bookBtn = document.querySelectorAll('.room-card--book-btn')
@@ -26,7 +39,7 @@ bookBtn.forEach((button) => {
 
 function toggleMenu() {
   if (bookingsDropdown.style.opacity === "0") {
-    bookingsDropdown.style.opacity = ".95"
+    bookingsDropdown.style.opacity = ".90"
     bookingsDropdown.style.pointerEvents = "all"
   } else {
     bookingsDropdown.style.opacity = "0"
@@ -36,8 +49,22 @@ function toggleMenu() {
 
 const booking = flatpickr(bookingForm, {
   enableTime: false,
+  dateFormat: "F J",
   mode: "range",
   minDate: "today",
   closeOnSelect: false,
   wrap: true
 });
+
+/*
+GOAL: To store a given booking as a class containing all relevant booking data. POST this data. Have the dropdown menu reflect the changes.
+
+DATA: Calendar data. Room data. User.
+  rooms (4): Residential Suite, Single Room, Junior Suite, Suite, 
+*/
+
+function loginUser(data) {
+  const randomUserIndex = Math.floor(Math.random() * data.customers.length)
+  store.userData = data.customers[randomUserIndex]
+  console.log(store.userData)
+}
