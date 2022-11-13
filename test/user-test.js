723d97b07b8981122df0444d03de3c7a69a167bf
James Wasmer <jameswasmer@gmail.com>
2022-11-13 13:46:20 -0500

    Add tests for Booking.js class file

---
+++ test/user-test.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+const expect = chai.expect;
+import User from '../src/classes/User.js'
+import { testCustomerData, testBookingsData, testRoomsData } from './data/test-data.js'
+
+describe('User.js class file', function() {
+  beforeEach()
+});
